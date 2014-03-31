0a523a5b677f4d30b69ed6cd5ace7d98c04d24e3
da1 <dyamada1988@gmail.com>
2014-03-31 19:25:37 +0900

    update

--- moneyPerMonth.py
+++ moneyPerMonth.py
@@ -15,11 +15,19 @@ class MoneyEntity:
         else:
             raise Exception, "unknown mode %s"%(mode)
 
-def moneyPerMonth(money):
+def getStrDate(tdatetime, mode):
+    if mode == "m":
+        return "%s-%s" % (tdatetime.year, tdatetime.month)
+    elif "4y":
+        return tdatetime.year if tdatetime.month > 3 else tdatetime.year - 1
+    else:
+        return tdatetime.year
+
+def moneyPerMonth(money, mode):
     perMonth = {}
     for m in money['money']:
         tdatetime = datetime.strptime(m['date'], '%Y-%m-%d')
-        strdate   = "%s-%s" % (tdatetime.year, tdatetime.month)
+        strdate = getStrDate(tdatetime, mode)
 
         if perMonth.has_key(strdate):
             perMonth[strdate].add(m['mode'], m['amount'])
@@ -31,15 +39,16 @@ def toArray(perMonthDic):
     return map(lambda date:perMonthDic[date], perMonthDic.keys())
 
 if __name__ == '__main__':
+    mode = "4y" # m or 4y or y
     zaim = Zaim()
     money = zaim.money()
-    perMonth = moneyPerMonth(money)
+    perMonth = moneyPerMonth(money, mode)
 
     perMonthArray = toArray(perMonth)
     perMonthArray.sort(key=operator.attrgetter('date'))
 
     print "date,payment,income"
     for entity in perMonthArray:
-        date = "%s-%s" % (entity.date.year, entity.date.month)
+        date = getStrDate(entity.date, mode)
         print "%s,%d,%d" % (date, entity.payment, entity.income)
 
